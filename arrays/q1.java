86d6c3e731b6291eacd2a50e13edf4c307ca58a9
sohum45 <142403572+sohum45@users.noreply.github.com>
2025-02-07 16:00:25 +0000

    dnn

---
+++ arrays/q1.java
@@ -0,0 +1,22 @@
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+// Note: The second largest element should not be equal to the largest element.
+
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int max =-1;
+        int smax =-1;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max){
+                smax = max;
+                max=arr[i];
+            }
+            else if(arr[i] > smax && max!=arr[i]){
+                smax=arr[i];
+            }
+        }
+        return smax;
+    }
+}
